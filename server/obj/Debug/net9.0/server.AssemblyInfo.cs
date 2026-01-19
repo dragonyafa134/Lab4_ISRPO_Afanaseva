4f5a57aad499a5939a36723225c7bc9448c814c3
dragonyafa134 <dasha.afanaseva.1206@mail.ru>
2026-01-19 16:00:04 +0300

    add gruppa and time (program.cs)

--- server/obj/Debug/net9.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net9.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91d00b024de7fae08473b2cf621811751d9e681a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8bf01ebbcce6c1dee0ff78ddef9c56a7210d02a")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- server/program.cs
+++ server/program.cs
@@ -1,3 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Привет");
 Console.WriteLine("Афанасьева Дн");
+Console.WriteLine("ИСП 233");
+Console.WriteLine(DateTime.Now);
