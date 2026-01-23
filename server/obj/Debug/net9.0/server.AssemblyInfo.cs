e9a5bb82321d0ebb355b503c79f3a0275019e7f3
dragonyafa134 <dasha.afanaseva.1206@mail.ru>
2026-01-23 16:51:48 +0300

    добавлены скриншоты

--- server/obj/Debug/net9.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net9.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a920d47687f2c2f295a9c565df93b4cde1a2fa9c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bac8a3c1a78cf8870b2f4bd6ad1a003c875b0b49")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
